c5bff7774468894c71be571090acc93d4172c3f5
Victor Nzyoka Mutunga <141123731+VictorNzyoka@users.noreply.github.com>
2024-10-03 11:30:00 +0300

    user role

--- api-database/controllers/DataBaseController.js
+++ api-database/controllers/DataBaseController.js
@@ -13,14 +13,16 @@ exports.register = async (req, res) => {
     const { name, lastName, gender, age, company, password, email } = req.body;
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        await axios.post(apiUrl, { name, lastName, gender, age, company, password: hashedPassword, email });
-        console.log("New user created !!")
+        const role = 'userRole'; 
+        await axios.post(apiUrl, { name, lastName, gender, age, company, password: hashedPassword, email, role });
+        console.log("New user created with role 'user'!!");
         res.status(200).json({ message: 'User created' });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(500).json({ message: 'Server error' });
     }
 };
+
 
 
 async function getClientByEmail(email) {
@@ -53,13 +55,14 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user[0].password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user[0].id, email: user[0].email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        const refreshToken = jwt.sign({ id: user[0].id, email: user[0].email }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+        const token = jwt.sign({ id: user[0].id, email: user[0].email, role: user[0].role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const refreshToken = jwt.sign({ id: user[0].id, email: user[0].email, role: user[0].role  }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
 
         const userInfo = {
             id: user[0].id,
             email: user[0].email,
             username: `${user[0].name} ${user[0].lastname}`,
+            userRole: user[0].role
         };
         console.log("User " + userInfo.username + " connected !")
         console.log(userInfo)
